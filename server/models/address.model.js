ae01bd4c01449d65f2c730a874a36912852f3aa2
saurabh kumar <saurabhslite2@gmail.com>
2025-02-21 12:09:56 +0530

    create order model

--- server/models/address.model.js
+++ server/models/address.model.js
@@ -24,6 +24,10 @@ const addressSchema = new mongoose.Schema(
       type: Number,
       default: null,
     },
+    status : {
+      type : Boolean,
+      default : true
+    }
   },
   {
     timestamps: true,
